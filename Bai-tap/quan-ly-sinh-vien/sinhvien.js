3bc33035206fe6e8e650d61bfbc49cdadc5b7b5e
locvankhoiC1020G1 <locvankhoi@gmail.com>
2021-03-26 08:12:46 +0700

    them bai tap

---
+++ Bai-tap/quan-ly-sinh-vien/sinhvien.js
@@ -0,0 +1,26 @@
+class Student {
+    constructor(_name,_toan, _ly,_hoa) {
+        this.name = _name;
+        this.toan= _toan;
+        this.ly = _ly;
+        this.hoa = _hoa;
+    }
+
+    addStudent(){
+    }
+    displayStudent(){
+
+    }
+    deleteStudent(){
+
+    }
+    addPoint(){
+
+    }
+    editPoint(){
+
+    }
+    countAvaragePoint(){
+
+    }
+}
